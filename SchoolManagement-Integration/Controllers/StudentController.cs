a6eadf37b0b3bf86f2887dcfdc98af0d93a5be00
abel masingita <abelmasingita9@gmail.com>
2024-10-08 01:27:44 +0200

    added web api to the project

---
+++ SchoolManagement-Integration/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagement_Integration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public StudentController()
+        {
+
+        }
+
+        [HttpGet(Name = "Get Students")]
+        public IEnumerable<Student> Get()
+        {
+            List<Student> student = new List<Student>();
+
+            return student;
+        }
+    }
+}
